a271e8027d77e0315fe7c3103444e14774a55313
koderoom <koderoom.dev@gmail.com>
2021-01-21 22:11:05 +0530

    t

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
+import { Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Route exact path="/" component={Page1} />
+
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+
+      <Route path="/page1" component={Page1} />
+      <Route path="/page2" component={Page2} />
+      <Route path="/page3" component={Page3} />
+    </>
+  );
+}
+
+function Login() {
+  return <div>Login</div>;
+}
+
+function Signup() {
+  return <div>Signup</div>;
+}
+
+function Page1() {
+  return <div>Page1</div>;
+}
+
+function Page2() {
+  return <div>Page2</div>;
+}
+
+function Page3() {
+  return <div>Page3</div>;
 }
 
 export default App;
